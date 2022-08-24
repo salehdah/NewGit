6dbecc5ce4fbd32bcbff35c48b88cea4fc335d5a
dzvisa1 <dzvisaa@gmail.com>
2022-08-23 20:49:11 -0400

    first commint

---
+++ src/operations/CallingMultiplication.java
@@ -0,0 +1,9 @@
+package operations;
+
+public class CallingMultiplication {
+    public static void main(String[] args) {
+        multiplication isa=new multiplication();
+        multiplication.multiplication(2,4);
+        System.out.println(isa);
+    }
+}
